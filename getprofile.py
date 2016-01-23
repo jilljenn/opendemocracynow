cbaa34400f9f29cedf9c25b52921e4e97feb0487
Jill-Jênn Vie <vie@jill-jenn.net>
2016-01-23 17:10:04 +0100

    Add profile generation

--- getprofile.py
+++ getprofile.py
@@ -1,12 +1,30 @@
 import csv
-def get_profile(user):
-    vote = []
+import jinja2
+import json
+
+with open('neighbors.json') as f:
+    neighbors = json.load(f)
+
+def get_votes(username):
+    votes = []
     with open('votes.csv', 'r', encoding='utf-8') as f:
         db = csv.DictReader(f, delimiter='\t')
         for row in db:
-            if row['username'] == user:
-                vote.append([row['vote'], row['article_link']])
-    return vote
+            if row['username'] == username:
+                votes.append([row['article_title'], row['article_link'], row['vote']])
+    return votes
 
-print(get_profile("vincentreverdy"))
+def get_neighbors(username):
+    return neighbors[username]
 
+def make_profile(username):
+    votes = get_votes(username)
+    neighbors = get_neighbors(username)
+    with open('web/profile/%s.html' % username, 'w') as f:
+        f.write(jinja2.Template(open('profile.html').read()).render({'votes': votes, 'neighbors': neighbors}))
+
+
+# make_profile("vincentreverdy")
+usernames = ["reyrobert", "meyermichel", "emmameunier", "brohanmickael", "alexmaluhia", "laurent6", "voltz", "dangienarnaud", "hersonjerome", "danielgohaud", "pierreemmanuelgr", "amenysbentabak", "johnrose", "hugoblanzat", "cedricblanchard"]
+for username in usernames:
+    print(make_profile(username))
